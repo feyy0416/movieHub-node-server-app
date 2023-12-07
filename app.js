c64d77a4c168a87c78dc1ca57f39f9e2c0c947f2
Zifei Song <song.zif@northeastern.edu>
2023-12-07 13:46:50 -0500

    added dotenv

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import session from "express-session";
 import CommentRoutes from './comments/routes.js';
 import LikeRoutes from './likes/routes.js';
 import FollowRoutes from './follows/routes.js';
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/movieHub';
+import "dotenv/config";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/moviehub';
+// mongoose.connect('mongodb://127.0.0.1:27017/moviehub');
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
@@ -15,17 +17,23 @@ app.use(
         origin: process.env.FRONTEND_URL,
     })
 );
+
 app.use(express.json());
 const port = process.env.PORT || 4000;
-
 const sessionOptions = {
     secret: "any string",
     resave: false,
     saveUninitialized: false,
 };
-app.use(
-    session(sessionOptions)
-);
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+
 CommentRoutes(app);
 UserRoutes(app);
 LikeRoutes(app);
